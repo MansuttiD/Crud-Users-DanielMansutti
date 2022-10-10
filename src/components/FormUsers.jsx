411505c109f08f9d09a4055d7aef5e76f772fd68
MansuttiD <daniel.mansutti@gmail.com>
2022-10-09 22:53:37 -0400

    height bugg resolved

--- src/components/FormUsers.jsx
+++ src/components/FormUsers.jsx
@@ -45,33 +45,33 @@ const FormUsers = ({createNewUser, updateUser, updateUserById, setUpdateUser, ha
   return (
 
     <div className='div__form' >
-    <button className='modal__close' onClick={resetModal}>X</button>
-    <form 
-    onSubmit={handleSubmit(submit)} className='form'
-    >
-        <h2 className='form__title'>{updateUser ? "Edit User" : 'New User'}</h2>
-        <div className='form__div'>
-            <label className='form__label' htmlFor="email">Email</label>
-            <input className='form__input' type="email" id="email" {...register('email')} />
-        </div>
-        <div className='form__div'>
-            <label className='form__label' htmlFor="password">Password</label>
-            <input className='form__input' type="password" id="password" {...register('password')}/>
-        </div>
-        <div className='form__div'>
-            <label className='form__label' htmlFor="first_name">First Name</label>
-            <input className='form__input' type="text" id="first_name" {...register('first_name')} />
-        </div>
-        <div className='form__div'>
-            <label className='form__label' htmlFor="last_name">Last Name</label>
-            <input className='form__input' type="text" id="last_name" {...register('last_name')}/>
-        </div>
-        <div className='form__div'>
-            <label className='form__label form__birthday'  htmlFor="birthday">Birthday</label>
-            <input className='form__input' type="date" id="birthday" {...register('birthday')} />
-        </div>
-        <button className='form__btn' onClick={handleModal}>{updateUser ? "Update" : "Create"}</button>
-    </form>
+        <button className='modal__close' onClick={resetModal}>X</button>
+        <form 
+        onSubmit={handleSubmit(submit)} className='form'
+        >
+            <h2 className='form__title'>{updateUser ? "Edit User" : 'New User'}</h2>
+            <div className='form__div'>
+                <label className='form__label' htmlFor="email">Email</label>
+                <input className='form__input' type="email" id="email" {...register('email')} />
+            </div>
+            <div className='form__div'>
+                <label className='form__label' htmlFor="password">Password</label>
+                <input className='form__input' type="password" id="password" {...register('password')}/>
+            </div>
+            <div className='form__div'>
+                <label className='form__label' htmlFor="first_name">First Name</label>
+                <input className='form__input' type="text" id="first_name" {...register('first_name')} />
+            </div>
+            <div className='form__div'>
+                <label className='form__label' htmlFor="last_name">Last Name</label>
+                <input className='form__input' type="text" id="last_name" {...register('last_name')}/>
+            </div>
+            <div className='form__div'>
+                <label className='form__label form__birthday'  htmlFor="birthday">Birthday</label>
+                <input className='form__input' type="date" id="birthday" {...register('birthday')} />
+            </div>
+            <button className='form__btn' onClick={handleModal}>{updateUser ? "Update" : "Create"}</button>
+        </form>
     </div>
   )
 }
